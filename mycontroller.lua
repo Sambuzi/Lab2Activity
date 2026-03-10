40d4f77fc2839a5d220c40a8dc0b2222c1929e81
Sambuzi <sajmir.buzi@studio.unibo.it>
2026-03-10 18:56:26 +0100

    added comments to explain the code better.

--- mycontroller.lua
+++ mycontroller.lua
@@ -1,9 +1,9 @@
+--costanti per il controllo del robot
 MAX_VELOCITY = 15
-
 BASE_SPEED = 10
 TURN_GAIN = 8
 AVOID_GAIN = 13
-
+--funzione per limitare un valore tra un minimo e un massimo
 function clamp(v, min_v, max_v)
     if v < min_v then
         return min_v
@@ -12,7 +12,7 @@ function clamp(v, min_v, max_v)
     end
     return v
 end
-
+--funzione per sommare i vettori dei sensori (luce o prossimità) e ottenere una direzione risultante
 function sum_vector(readings)
     local x, y = 0, 0
     for i = 1, #readings do
@@ -42,13 +42,12 @@ function step()
     local tx = lx - 1.8 * px
     local ty = ly - 1.8 * py
     local angle = math.atan2(ty, tx)
-
+    -- Normalizza l'angolo e calcola la velocità di rotazione e di avanzamento(in avanti).
     local turn = clamp(angle / (math.pi / 2), -1, 1)
     local forward = clamp(BASE_SPEED - AVOID_GAIN * avoid, 3, BASE_SPEED)
-
+    --setto le velocità delle ruote in base alla direzione e alla velocità desiderata
     local left = clamp(forward - TURN_GAIN * turn, -MAX_VELOCITY, MAX_VELOCITY)
     local right = clamp(forward + TURN_GAIN * turn, -MAX_VELOCITY, MAX_VELOCITY)
-
     robot.wheels.set_velocity(left, right)
 end
 
